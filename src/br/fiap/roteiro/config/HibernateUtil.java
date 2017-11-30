fccb4828c694ee347b92ea92fd3c54f4e26f0d04
Higor Sapo <higor.xsapo@gmail.com>
2017-11-30 12:31:44 -0200

    Classes de pedidos e clientes

---
+++ src/br/fiap/roteiro/config/HibernateUtil.java
@@ -0,0 +1,24 @@
+package br.fiap.roteiro.config;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.stat.SessionStatistics;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+
+
+	static {
+		try {
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+
+}
